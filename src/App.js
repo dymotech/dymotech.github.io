a0988094f3d06e7fc94493ed3f48e12f48303764
Cody Martin <csmartin1024@gmail.com>
2018-07-29 14:52:52 -0700

    Trigger

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends Component
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Dymo's Site (Now building with Circle CI)</h1>
+          <h1 className="App-title">Welcome to Dymo's Site (Now building with Circle CI Finally)</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
